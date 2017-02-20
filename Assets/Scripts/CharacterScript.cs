39a7791ef8547aa44396a19c94c67902ae7b6518
daniel-geffen <geffen.daniel@gmail.com>
2017-02-20 18:23:45 +0200

    Basic Character

---
+++ Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour {
+
+    public float speed;
+    public float jumpForce;
+    public LayerMask groundLayer;
+    public float groundCheckRadius;
+    bool onGround = true;
+    bool facingRight = true;
+    Rigidbody2D r;
+
+	void Start () {
+        r = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+		
+	}
+
+    void FixedUpdate() {
+        float move = Input.GetAxis("Horizontal");
+        r.velocity = new Vector2(move * speed, r.velocity.y);
+
+        if (move < 0 && facingRight || move > 0 && !facingRight)
+            ChangeDirection();
+
+        Vector2 characterPosition = r.position;
+        characterPosition.y -= 1;
+        onGround = Physics2D.OverlapCircle(characterPosition, groundCheckRadius, groundLayer);
+
+        if (onGround && Input.GetKeyDown(KeyCode.Space)) 
+            r.AddForce(new Vector2(0, jumpForce));
+    }
+
+    void ChangeDirection() {
+        facingRight = !facingRight;
+        Vector3 characterScale = transform.localScale;
+        characterScale.x *= -1;
+        transform.localScale = characterScale;
+    }
+}
