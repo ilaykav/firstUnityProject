107a94ec919db9c046f939630535657c60fe89f0
ilaykav <trumpet.ilay@gmail.com>
2017-03-11 12:49:40 +0200

    buggy score and powerups

---
+++ Assets/Scripts/HUDScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDScript : MonoBehaviour {
+
+    // Use this for initialization
+    float playerScore = 0;
+
+    public void increaseScore (int amount) {
+        playerScore += amount;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerScore += Time.deltaTime;
+	}
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 30), "Score: " + (int)(playerScore * 100));
+    }
+}
---
+++ Assets/Scripts/PowerUpScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PowerUpScript : MonoBehaviour
+{
+    HUDScript hud;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            hud = GameObject.Find("Camera").GetComponent<HUDScript>();
+            hud.increaseScore(10);
+            Destroy(this.gameObject);
+        }
+    }
+}
